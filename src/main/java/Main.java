f19ad90ba0280854d536362fdac6953792db0133
VikiLeo <viktoria_leon@mail.ru>
2023-07-07 17:09:58 +0300

    итоговая работа 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
              Scanner scanner = new Scanner(System.in);
-        System.out.println("На сколько человек необходимо разделить счет 1231?");
+        System.out.println("На сколько человек необходимо разделить счет?");
              Product foodBasket = new Product();
              foodBasket.basicProgram();
     }
